a335ef7e47bded63f0e3dde13179ea3ec1664d99
Ajit Kumar <kajit1134@gmail.com>
2026-01-19 00:28:19 +0530

    updated rasa

--- rasa_bot/actions/actions.py
+++ rasa_bot/actions/actions.py
@@ -4,6 +4,7 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 from rasa_sdk import Tracker
 from typing import Any, Text, List, Dict
+from datetime import datetime
 
 class ActionTellJoke(Action):
 
@@ -19,4 +20,17 @@ class ActionTellJoke(Action):
 
         joke = pyjokes.get_joke(category="neutral", language="en")
         dispatcher.utter_message(text=joke)
+        return []
+
+class ActionTellDateTime(Action):
+    def name(self):
+        return "action_tell_datetime"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker, domain):
+        now = datetime.now()
+        formatted = now.strftime("%A, %d %B %Y, %I:%M %p")
+
+        dispatcher.utter_message(
+            text=f"🕒 The current date and time is {formatted}"
+        )
         return []
